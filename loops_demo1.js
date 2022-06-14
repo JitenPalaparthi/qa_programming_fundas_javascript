113c792bc1ff2494267f8c2e3fb396f7e8520d91
Jiten Palaparthi <JPalaparthi@vmware.com>
2022-06-14 19:48:39 +0530

    Add loops example

---
+++ loops_demo1.js
@@ -0,0 +1,29 @@
+// 1- 10
+
+// 1. Initial Expression
+// 2. Condition Expression
+// 3. Increment Expression
+
+/*var i =1 ; //1
+while(i<=10){ //2
+    console.log(i); // statements or logic to execute
+i++;
+}*/
+
+/*let n = 1 ;
+while(n<=100){
+    if(n%2==0){
+        console.log(n," is even number");
+    }else{
+        console.log(n," is odd number");
+    }
+    n++;
+}*/
+
+for(var i=1;i<=10;i++){
+    console.log(i);
+}
+
+// for-in for-of 
+// for/in loop is used for iterating through the properties of an object
+// for/of if any object is iterable 
